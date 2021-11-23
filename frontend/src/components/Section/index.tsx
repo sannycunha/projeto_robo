69ad7e4ffb064b65f8b5617601a12b02d7ae13ef
Sannycunha <sannycunha01@gmail.com>
2021-11-23 19:36:11 -0300

    Alterações Quem Somos e Section index

--- frontend/src/components/Section/index.tsx
+++ frontend/src/components/Section/index.tsx
@@ -1,6 +1,10 @@
 import fabio from 'assets/fotos/foto_fabio.jpeg';
 import gleici from 'assets/fotos/foto_gleici.jpeg';
 import raquel from 'assets/fotos/foto_raquel.jpeg';
+import felipe from 'assets/fotos/foto_felipe.jpeg';
+import advanilson from 'assets/fotos/foto_advanilson.jpeg';
+import katia from 'assets/fotos/foto_katia.jpeg';
+import augusto from 'assets/fotos/foto_augusto.jpeg';
 function Section() {
   return (
     <div className="conteiner">
@@ -52,13 +56,13 @@ function Section() {
       <div className="card mb-3 ml-5 card-studens-outside">
         <div className="row no-gutters card card-studens">
           <div className="col-md-1">
-            <img src={fabio} className="card-img rounded-circle m-2 mt-0 pt-0" alt="Fabio D.Sinobre" />
+            <img src={felipe} className="card-img rounded-circle m-2 mt-0 pt-0" alt="Felipe da Silva Cruz" />
           </div>
           <div className="col-md-11 ">
             <div className="card-body text-primary">
-              <h5 className="card-title"><b>Fábio D. Sinobre</b></h5>
-              <p className="card-text">Natural de Guarulhos SP, gosto de aprender coisas novas, amante de tecnologia meu principal “hobby” é astronomia amadora fiz curso no IAG de astronomia amadora e sempre que posso participo de eventos da área como ENAST.</p>
-              <p className="card-text">Estudei até o quinto semestre no IF-SP no curso de licenciatura em física, em 2018 iniciei o curso de bacharel em engenharia da computação na UNIVESP</p>
+              <h5 className="card-title"><b>Felipe da Silva Cruz</b></h5>
+              <p className="card-text">Natural de Guarulhos-SP, estudante de Engenharia da Computação.  A paixão por tecnologia começou desde aos sete anos de idade, quando ganhei meu primeiro computador com o sistema Windows 95.</p>
+              <p className="card-text">Logo em seguida aprofundei-me em Computação Gráfica e até hoje tenho sede por conhecimento por novas tecnologias e também em desenvolvê-las.</p>
               <p className="card-text text-center"><small className="text-primary">Guarulhos, 20 de Outubro de 2021</small></p>
             </div>
           </div>
@@ -67,13 +71,13 @@ function Section() {
       <div className="card mb-3 ml-5 card-studens-outside" >
         <div className="row no-gutters card card-studens">
           <div className="col-md-1">
-            <img src={fabio} className="card-img rounded-circle m-2 mt-0 pt-0" alt="Fabio D.Sinobre" />
+            <img src={advanilson} className="card-img rounded-circle m-2 mt-0 pt-0" alt="Advanilson Neri de Sousa" />
           </div>
           <div className="col-md-11 ">
             <div className="card-body text-primary">
-              <h5 className="card-title"><b>Fábio D. Sinobre</b></h5>
-              <p className="card-text">Natural de Guarulhos SP, gosto de aprender coisas novas, amante de tecnologia meu principal “hobby” é astronomia amadora fiz curso no IAG de astronomia amadora e sempre que posso participo de eventos da área como ENAST.</p>
-              <p className="card-text">Estudei até o quinto semestre no IF-SP no curso de licenciatura em física, em 2018 iniciei o curso de bacharel em engenharia da computação na UNIVESP</p>
+              <h5 className="card-title"><b>Advanilson Neri de Sousa</b></h5>
+              <p className="card-text">Natural de São Paulo-SP, Acredito que a compreensão do mundo é por meio da curiosidade. Adoro saber como as coisas funcionam e fazer uso do ócio produtivo para renovar as energias.</p>
+              <p className="card-text">Ler, debater e investir é um modo de vida simples.  Como hobby corrida de rua e andar de bicicleta aos finais de semana.</p>
               <p className="card-text text-center"><small className="text-primary">Guarulhos, 20 de Outubro de 2021</small></p>
             </div>
           </div>
@@ -82,13 +86,13 @@ function Section() {
       <div className="card mb-3 ml-5 card-studens-outside">
       <div className="row no-gutters card card-studens">
           <div className="col-md-1">
-            <img src={fabio} className="card-img rounded-circle m-2 mt-0 pt-0" alt="Fabio D.Sinobre" />
+            <img src={katia} className="card-img rounded-circle m-2 mt-0 pt-0" alt="Katia Ferreira de Sousa Ribeiro" />
           </div>
           <div className="col-md-11 ">
             <div className="card-body text-primary">
-              <h5 className="card-title"><b>Fábio D. Sinobre</b></h5>
-              <p className="card-text">Natural de Guarulhos SP, gosto de aprender coisas novas, amante de tecnologia meu principal “hobby” é astronomia amadora fiz curso no IAG de astronomia amadora e sempre que posso participo de eventos da área como ENAST.</p>
-              <p className="card-text">Estudei até o quinto semestre no IF-SP no curso de licenciatura em física, em 2018 iniciei o curso de bacharel em engenharia da computação na UNIVESP</p>
+              <h5 className="card-title"><b>Katia Ferreira de Sousa Ribeiro</b></h5>
+              <p className="card-text">Natural de São Miguel SP, Sou estudante de Engenharia da Computação, apaixonada por tecnologia.</p>
+              <p className="card-text">Nas horas vagas gosto de viajar, sair com minha família e aprender coisas novas.</p>
               <p className="card-text text-center"><small className="text-primary">Guarulhos, 20 de Outubro de 2021</small></p>
             </div>
           </div>
@@ -97,13 +101,13 @@ function Section() {
       <div className="card mb-3 ml-5 card-studens-outside">
         <div className="row no-gutters card card-studens">
           <div className="col-md-1">
-            <img src={fabio} className="card-img rounded-circle m-2 mt-0 pt-0" alt="Fabio D.Sinobre" />
+            <img src={augusto} className="card-img rounded-circle m-2 mt-0 pt-0" alt="Augusto Santos" />
           </div>
           <div className="col-md-11 ">
             <div className="card-body text-primary">
-              <h5 className="card-title"><b>Fábio D. Sinobre</b></h5>
-              <p className="card-text">Natural de Guarulhos SP, gosto de aprender coisas novas, amante de tecnologia meu principal “hobby” é astronomia amadora fiz curso no IAG de astronomia amadora e sempre que posso participo de eventos da área como ENAST.</p>
-              <p className="card-text">Estudei até o quinto semestre no IF-SP no curso de licenciatura em física, em 2018 iniciei o curso de bacharel em engenharia da computação na UNIVESP</p>
+              <h5 className="card-title"><b>Augusto Santos</b></h5>
+              <p className="card-text">Natural Vale do Paraiba-SP,estudante de curso de Engenharia da Computação,  questionador, pesquisador, entusiasta de tecnologia e áreas correlatas.  Aviação e astronomia são temas para longas conversas, bem como rádio e comunicação.</p>
+              <p className="card-text">Facilitar o acesso à educação, conhecimento e a cultura é o primeiro passo para que haja uma sociedade mais sábia.</p>
               <p className="card-text text-center"><small className="text-primary">Guarulhos, 20 de Outubro de 2021</small></p>
             </div>
           </div>
